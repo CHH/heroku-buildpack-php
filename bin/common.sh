3ce7ec911123d3ce9e2e0333db39a23bf1e604ec
Fredrik Wollsén <fredrik@neam.se>
2014-05-15 10:15:38 +0300

    Make sure that php-fpm can write to the log fifos (user = nobody)

--- bin/common.sh
+++ bin/common.sh
@@ -40,6 +40,8 @@ init_log_plex_fifo() {
   for log_file in $*; do
     echo "rm -f ${log_file}"
     echo "mkfifo ${log_file}"
+    echo "# make sure that php-fpm can write to the log fifos (user = nobody)"
+    echo "chown nobody: ${log_file}"
   done
 }
 
